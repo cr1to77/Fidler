b5a16ea421274e82232ccfa85bbb3ac5ed1c6c00
cr1to77 <criton@fidlerindustrial.com.au>
2025-06-11 11:29:47 +1000

    Update websocket-server.js

--- websocket-server.js
+++ websocket-server.js
@@ -1,4 +1,3 @@
-// websocket-server.js
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: process.env.PORT || 10000 });
 
@@ -6,19 +5,18 @@ let clients = [];
 
 wss.on('connection', (ws) => {
   clients.push(ws);
-
-  ws.on('message', (message) => {
-    // Broadcast to all connected clients except the sender
-    for (let client of clients) {
+  ws.on('message', (msg) => {
+    // Broadcast to all other clients
+    for (const client of clients) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(msg);
       }
     }
   });
 
   ws.on('close', () => {
-    clients = clients.filter(client => client !== ws);
+    clients = clients.filter(c => c !== ws);
   });
 });
 
-console.log('WebSocket server running...');
+console.log("WebSocket server running...");
